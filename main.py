448e1cdaf19d60c4a9d3b0d6af6ee1aca20bcf26
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-20 18:58:16 +0500

    database setup

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
+from database import models
+from database.database import engine
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+models.Base.metadata.create_all(engine)
