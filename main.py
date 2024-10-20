041e593388c6ae1cd0846a74c2681d644ffcd682
Adilet Shildebayev <adilet.shildebayev@nu.edu.kz>
2024-10-20 19:05:20 +0500

    Create Post

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database import models
 from database.database import engine
-
+from routers import post
 app = FastAPI()
-
+app.include_router(post.router)
 models.Base.metadata.create_all(engine)
